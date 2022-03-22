b4b4b04562c644ecc6a7ef699ea07076b7a7cf76
Mahendra Sutar <mahendrasutar8.ms@gmail.com>
2022-03-22 21:46:19 +0530

    Completed Doubly Linked List

--- doubly_linked_list.java
+++ doubly_linked_list.java
@@ -29,6 +29,101 @@ class DoublyLinkedList {
         }
         
     }
+    public void insertDataAtFront(int data){
+        Node currNode = head;
+        Node new_node = new Node(data);
+        if(head == null){
+            head = new_node;
+            new_node.next = null;
+            new_node.prev = null;
+            return;
+        }
+        head.prev = new_node;
+        new_node.prev = null;
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void insertDataAtIndex(int data, int Index){
+        Node new_node = new Node(data);
+        Node currNode = head,prev=null;
+        if(head == null){
+            head = new_node;
+            new_node.next = null;
+            new_node.prev = null;
+            return;
+        }
+        int counter = 0;
+        do{
+            if(counter == Index){
+                prev.next = new_node;
+                new_node.next = currNode;
+                new_node.prev = prev;
+                currNode.prev = new_node;
+                return;
+            }
+            else{
+                prev = currNode;
+                currNode = currNode.next;
+                counter++;
+            }
+        }while(currNode != null);
+
+        prev.next = new_node;
+        new_node.next = null;
+        new_node.prev = prev;
+    }
+
+    public void deleteFromFront(){
+        if(head == null){
+            return;
+        }
+        head = head.next;
+        head.prev = null;
+    }
+    public void deleteFromLast(){
+        Node currNode = head,prev=null;
+        if(head == null){
+            return;
+        }
+        while(currNode.next != null){
+            prev = currNode;
+            currNode = currNode.next;
+        }
+        prev.next = null;
+        
+    }
+
+    public void deleteFromIndex(int index){
+        Node currNode = head,prev=null,currNodeNext=null;
+        if(head == null){
+            return;
+        }
+        if(index == 0){
+            head = currNode.next;
+            head.prev = null;
+            return;
+        }
+        int counter = 0;
+        do{
+            if(counter == index){
+                if(currNode.next == null){
+                    prev.next = null;
+                    return;
+                }
+                prev.next = currNode.next;
+                currNodeNext = currNode.next;
+                currNodeNext.prev = prev;
+                return;
+            }
+            else{
+                prev = currNode;
+                currNode = currNode.next;
+                counter++;
+            }
+        }while(currNode != null);
+        return;
+    }
     public void printList(){
         Node currNode = head;
         do{
@@ -45,5 +140,29 @@ class DoublyLinkedList {
         list.insertData(40);
 
         list.printList();
+
+        // list.insertDataAtFront(50);
+        // list.insertDataAtFront(60);
+        // System.out.println();
+        // list.printList();
+
+        // list.insertDataAtIndex(60,3);
+        // System.out.println();
+        // list.printList();
+
+        // list.deleteFromFront();
+        // list.deleteFromFront();
+        // System.out.println();
+        // list.printList();
+
+        // list.deleteFromLast();
+        // list.deleteFromLast();
+        // System.out.println();
+        // list.printList();
+
+        list.deleteFromIndex(3);
+        System.out.println();
+        list.printList();
+
     }
 }
